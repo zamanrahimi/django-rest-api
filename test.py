7e5e0ef812cf74c4721a22b2e254a642289d84ee
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-17 02:09:04 +0500

    , - ,The file test1.py has been committed

---
+++ test.py
@@ -0,0 +1,22 @@
+
+import os
+import time
+
+# pull first 
+
+for i in range (1,5000):
+
+    # push 
+    f = time.sleep(5) 
+    os.system(r"C:\Users\test\Desktop\test\test\m.bat")
+
+    # pull
+    os.system(r"C:\Users\test\Desktop\test\test\pull.bat")
+    f = time.sleep(5)    # pauses for 5 seconds
+    o = open("test1.py", "w")
+    o.write("{} print('This is a Django Rest Framework project')".format(i))
+
+
+
+
+
---
+++ test1.py
@@ -0,0 +1,1 @@
+4461 print('This is a Python file for testing pupose')
