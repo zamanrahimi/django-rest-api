7e87ee231a13a7738850fdf43ece47f32d45d1b3
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-17 10:33:57 +0500

    , - ,The file test1.py has been committed

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ import time
 for i in range (1,5000):
 
     # push 
-    f = time.sleep(5) 
+    f = time.sleep(1) 
     os.system(r"C:\Users\test\Desktop\test\test\m.bat")
 
     # pull
     os.system(r"C:\Users\test\Desktop\test\test\pull.bat")
-    f = time.sleep(5)    # pauses for 5 seconds
+    f = time.sleep(1)    # pauses for 5 seconds
     o = open("test1.py", "w")
     o.write("{} print('This is a Django Rest Framework project')".format(i))
 
